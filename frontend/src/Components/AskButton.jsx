626d01e2856dcd39e6817ab4768e88e0fe7673b5
SandeSydney <sandesydney01@gmail.com>
2022-12-02 22:57:06 +0300

    Added navigation

--- frontend/src/Components/AskButton.jsx
+++ frontend/src/Components/AskButton.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import add from '../Resources/add.png'
 
 function AskButton() {
+    const navigate = useNavigate()
+    const ask = ()=>{
+        navigate('/home/ask-question')
+    }
     return (
-        <div className="ask">
+        <div className="ask" onClick={ask}>
             <img className='action-button' src={add} alt="add question" />
-            <p>Ask</p>
+            {/* <p>Ask</p> */}
         </div>
     )
 }
