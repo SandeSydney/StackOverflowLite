9d5431c717f44ac17ee687efc6626164042ac1ca
SandeSydney <sandesydney01@gmail.com>
2022-12-02 20:40:50 +0300

    Added ask button's navigation

--- frontend/src/Components/Sidenav.jsx
+++ frontend/src/Components/Sidenav.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Sidenav() {
+    const navigate = useNavigate()
+
+    const askquestion = ()=>{
+        navigate("/home/ask-question")
+    }
     return (
         <>
-            <button className='homepage-btn'>
+            <button className='homepage-btn' >
                 My Questions
             </button>
-            <button className='homepage-btn'>
+            <button className='homepage-btn' onClick={askquestion}>
                 Ask Question
             </button>
         </>
