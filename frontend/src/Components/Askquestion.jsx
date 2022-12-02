06c0f90a09d8cfbeaf24ad9d037b0de0690550ee
SandeSydney <sandesydney01@gmail.com>
2022-12-02 22:58:26 +0300

    Modified the back button

--- frontend/src/Components/Askquestion.jsx
+++ frontend/src/Components/Askquestion.jsx
@@ -4,14 +4,13 @@ import back from '../Resources/back.png'
 
 function Askquestion() {
     const navigate = useNavigate()
-    const handleBack = ()=>{
+    const handleBack = () => {
         navigate("/home")
     }
     return (
         <div className='form-div'>
             <div className="back-div" onClick={handleBack}>
                 <img className='btnBack' src={back} alt="Go Back" />
-                <p>Back</p>
             </div>
             <p>You have a question? Type it in the form and submit is it to get responses.</p>
             <form className='ask-form'>
