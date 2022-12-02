1df527695d0f349a8065b627d826760b6c99c7fa
SandeSydney <sandesydney01@gmail.com>
2022-12-02 10:56:05 +0300

    Modified the file

--- frontend/src/Components/Questionbox.jsx
+++ frontend/src/Components/Questionbox.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import userimg from '../Resources/user.png'
+import userimg2 from '../Resources/user6.jpeg'
 
 function Questionbox() {
   return (
-    <div className='question-box'>
-      <div className="user-details">
-        <img src={userimg} alt="user profile pic" />
-        <p>John</p>
-        <p>Doe</p>
+    <>
+      <div type='button' className='question-box'>
+        <div className="user-details">
+          <img src={userimg} alt="user profile pic" />
+          <p>John</p>
+          <p>Doe</p>
+        </div>
+        <div className="qtnAns">
+          <p className='question'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident odio, animi illum laudantium ratione sunt expedita reiciendis fugiat sint dolores tempora, deleniti atque nam repudiandae praesentium ut beatae quibusdam nesciunt.
+          </p>
+          <button className='answer-link'>Answers (3)</button>
+        </div>
       </div>
-      <div className="qtnAns">
-        <p className='question'>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident odio, animi illum laudantium ratione sunt expedita reiciendis fugiat sint dolores tempora, deleniti atque nam repudiandae praesentium ut beatae quibusdam nesciunt.
-        </p>
-        <p className='answer-link'>Answers (3)</p>
-      </div>
-
-    </div>
+    </>
   )
 }
 
