57dd311a443671b9c12b9d7581804d3d9ea4c8f8
SandeSydney <sandesydney01@gmail.com>
2022-12-02 10:56:59 +0300

    Modified homepage

--- frontend/src/Components/Homepage.jsx
+++ frontend/src/Components/Homepage.jsx
@@ -1,30 +1,58 @@
 import React from 'react'
 import logo from '../Resources/Logo.png'
-import menu from '../Resources/menubar.png'
 import AskButton from './AskButton'
 import Questionbox from './Questionbox'
-import QuestionDiv from './Questionbox'
 import SearchBox from './SearchBox'
+import user from '../Resources/user.png'
+import Sidenav from './Sidenav'
+import Menubutton from './Menubutton'
 
 
 function Homepage() {
     return (
-        <div className='container'>
+        <div className='container-main'>
             <nav>
                 <img src={logo} alt="logo" />
-                <img className='action-button' src={menu} alt="menu bar" />
+                <div className="links">
+                    <a href='/'>Home</a>
+                    <a href='/'>Most Upvoted</a>
+                    <a href='/'>Most Answered</a>
+                </div>
+                <div>
+                    <div className="search-profile">
+                        <SearchBox />
+                        <div className="profile">
+                            <img src={user} alt="Profile pic" />
+                            <p>John Doe</p>
+                        </div>
+                    </div>
+                    <Menubutton />
+                </div>
             </nav>
             <div className="action-nav">
                 <AskButton />
                 <SearchBox />
             </div>
-            <div className="log-content main-content">
-                <Questionbox />
-                <Questionbox />
-                <Questionbox />
-                <Questionbox />
-                <Questionbox />
-                <Questionbox />
+            <div className='home-display'>
+                <div className="side-nav">
+                    <Sidenav />
+                </div>
+                <div className="main-content">
+                    <Questionbox />
+                    <Questionbox />
+                    <Questionbox />
+                    <Questionbox />
+                    <Questionbox />
+                    <Questionbox />
+                    <Questionbox />
+                    <Questionbox />
+                    <Questionbox />
+                    <Questionbox />
+                    <Questionbox />
+                </div>
+                <div className="extra">
+
+                </div>
             </div>
         </div>
     )
