b172e8e5febe1fa6b613f84751834b5bc6f7b6fa
SandeSydney <sandesydney01@gmail.com>
2022-12-02 20:40:22 +0300

    Modified linking

--- frontend/src/Components/Menubutton.jsx
+++ frontend/src/Components/Menubutton.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import menu from '../Resources/menubar.png'
 import Dropdown from 'react-bootstrap/Dropdown'
+import { Link } from 'react-router-dom'
 
 function Menubutton() {
     return (
@@ -9,9 +10,18 @@ function Menubutton() {
                 <img className='action-button navimg' src={menu} alt="menu bar" />
             </Dropdown.Toggle>
             <Dropdown.Menu>
-                <Dropdown.Item href='/'>Home</Dropdown.Item>
-                <Dropdown.Item href='/'>Most Upvoted</Dropdown.Item>
-                <Dropdown.Item href='/'>Most Answered</Dropdown.Item>
+                <Dropdown.Item>
+                    <Link to={'#'}>Home</Link>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                    <Link to={'#'}>Most Upvoted</Link>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                    <Link to={'#'}>Most Answered</Link>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                    <Link to={'/'}>Logout</Link>
+                </Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
     )
