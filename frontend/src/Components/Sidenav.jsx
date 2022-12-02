40b7189d239c483de81c0c84702770fb906ed805
SandeSydney <sandesydney01@gmail.com>
2022-12-02 10:55:38 +0300

    Added Side Navigation

---
+++ frontend/src/Components/Sidenav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Sidenav() {
+    return (
+        <>
+            <button className='homepage-btn'>
+                My Questions
+            </button>
+            <button className='homepage-btn'>
+                Ask Question
+            </button>
+        </>
+    )
+}
+
+export default Sidenav
