3bc6e0da548355aacab21da1dc68fb5b6320b484
SandeSydney <sandesydney01@gmail.com>
2022-12-02 22:56:56 +0300

    Added navigation and back button

--- frontend/src/Components/Askquestion.jsx
+++ frontend/src/Components/Askquestion.jsx
@@ -1,30 +1,40 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import back from '../Resources/back.png'
 
 function Askquestion() {
-  return (
-    <div className='form-div'>
-        <p>You have a question? Type it in the form and submit is it to get responses.</p>
-        <form>
-            <div className="form-element">
-                <label htmlFor="Subject">Subject:</label>
-                <select name="subject" id="subject">
-                    <option value="engineering">Engineering</option>
-                    <option value="medicine">Medicine</option>
-                    <option value="agriculture">Agriculture</option>
-                    <option value="software dev">Software Dev.</option>
-                </select>
+    const navigate = useNavigate()
+    const handleBack = ()=>{
+        navigate("/home")
+    }
+    return (
+        <div className='form-div'>
+            <div className="back-div" onClick={handleBack}>
+                <img className='btnBack' src={back} alt="Go Back" />
+                <p>Back</p>
             </div>
-            <div className="form-element">
-                <label htmlFor="question">Question:</label>
-                <textarea name="question" id="question-textarea" cols="30" rows="10" placeholder='Type your question here...'></textarea>
-            </div>
-            <div className="formBtns">
-                <button className='btnReset' type="reset">Reset</button>
-                <button className='btnSubmit' type="submit">Submit</button>
-            </div>
-        </form>
-    </div>
-  )
+            <p>You have a question? Type it in the form and submit is it to get responses.</p>
+            <form className='ask-form'>
+                <div className="form-element">
+                    <label htmlFor="Subject">Subject:</label>
+                    <select name="subject" id="subject">
+                        <option value="engineering">Engineering</option>
+                        <option value="medicine">Medicine</option>
+                        <option value="agriculture">Agriculture</option>
+                        <option value="software dev">Software Dev.</option>
+                    </select>
+                </div>
+                <div className="form-element">
+                    <label htmlFor="question">Question:</label>
+                    <textarea name="question" id="question-textarea" cols="30" rows="8" placeholder='Type your question here...'></textarea>
+                </div>
+                <div className="formBtns">
+                    <button className='btnReset' type="reset">Reset</button>
+                    <button className='btnSubmit' type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Askquestion
