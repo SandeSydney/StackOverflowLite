9370e12a2d9f8e9c7ec335865784fcfdeaa411f5
SandeSydney <sandesydney01@gmail.com>
2022-12-02 20:38:49 +0300

    Update on container in main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Homepage from './Components/Homepage';
 
 function App() {
   return (
-    <div className="App">
-      App 
+    <div>
+      <Homepage />
     </div>
   );
 }
