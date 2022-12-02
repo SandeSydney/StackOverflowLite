8e7d81040bd20711c7f93107f0607feec2d87344
SandeSydney <sandesydney01@gmail.com>
2022-12-02 20:39:00 +0300

    Added routing to app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {RouterProvider} from 'react-router-dom'
+import { router } from './Routing/RouteComp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
