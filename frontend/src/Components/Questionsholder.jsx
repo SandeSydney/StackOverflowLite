4dd8e4dfae5975272d61dd60d57c6d5698f8ded9
SandeSydney <sandesydney01@gmail.com>
2022-12-02 22:56:08 +0300

    Created container to hold questions

---
+++ frontend/src/Components/Questionsholder.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Questionbox from './Questionbox'
+
+function Questionsholder() {
+    return (
+        <div className="questions-holder">
+            <h2>All Questions</h2>
+            <Questionbox />
+            <Questionbox />
+            <Questionbox />
+            <Questionbox />
+            <Questionbox />
+            <Questionbox />
+            <Questionbox />
+            <Questionbox />
+            <Questionbox />
+            <Questionbox />
+            <Questionbox />
+            <Questionbox />
+            <Questionbox />
+            <Questionbox />
+            <Questionbox />
+            <Questionbox />
+        </div>
+    )
+}
+
+export default Questionsholder
