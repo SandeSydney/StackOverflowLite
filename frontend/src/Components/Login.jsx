9f24b577b802ea2f2f66e12ea00b2f0d8610215b
SandeSydney <sandesydney01@gmail.com>
2022-12-02 20:40:10 +0300

    Added navigation

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import logo from '../Resources/Logo.png'
 
 function Login() {
+    const navigate = useNavigate()
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        navigate("/home")
+    }
+
+    const handleSignup = (event)=>{
+        event.preventDefault();
+        navigate("/signup")
+    }
+
     return (
         <div className='logContainer'>
             <div className="logNav">
@@ -17,7 +30,7 @@ function Login() {
 
                     <p className='no-acc'>
                         No Account? No Problem. <br />
-                        Sign Up <span className='shouter-2'>Here!</span>
+                        Sign Up <span className='shouter-2' onClick={handleSignup}>Here!</span>
                     </p>
                 </div>
                 <form>
@@ -30,11 +43,13 @@ function Login() {
                         <label htmlFor="pass">Password:</label>
                         <input type="password" />
                     </div>
-                    <button type="submit" className='btnSubmit loginSubmit'>Login</button>
+                    <button type="submit" className='btnSubmit loginSubmit' onClick={handleLogin}>
+                        Login
+                    </button>
 
                 </form>
                 <p className="logRequest-2">
-                    No Account Yet? Sign Up <span className='toSign'>Here!</span>
+                    No Account Yet? Sign Up <span className='toSign' onClick={handleSignup}>Here!</span>
                 </p>
             </div>
         </div>
