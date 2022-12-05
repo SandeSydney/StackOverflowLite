add6f6a0813ee0afac45dff38adfa0c25584b7c1
SandeSydney <sandesydney01@gmail.com>
2022-12-05 07:17:06 +0300

    Added voting sections

--- frontend/src/Components/Answerbox.jsx
+++ frontend/src/Components/Answerbox.jsx
@@ -3,7 +3,8 @@ import userimg2 from '../Resources/user6.jpeg'
 
 function Answerbox() {
   return (
-    <div className='answer-box'>
+    <div className="answer-main">
+      <div className='answer-box'>
         <div className='qtnAns'>
           <p className='answer'>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident odio, animi illum laudantium ratione sunt expedita reiciendis fugiat sint dolores tempora, deleniti atque nam repudiandae praesentium ut beatae quibusdam nesciunt.
@@ -17,6 +18,12 @@ function Answerbox() {
           <p>Jane Doe</p>
         </div>
       </div>
+      <div className="answer-votes">
+        <p>Upvote</p>
+        <p>Downvote</p>
+        <p>Comment</p>
+      </div>
+    </div>
   )
 }
 
