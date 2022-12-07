c5b9b9398f00ea371f04c020b70ba64ff980c8f4
SandeSydney <sandesydney01@gmail.com>
2022-12-07 15:44:26 +0300

    Added questioncontroller

---
+++ backend/controllers/questionController.js
@@ -0,0 +1,38 @@
+const getQuestions = async(req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const getSingleQuestion = async(req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const addUpdateQuestion = async(req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const deleteQuestion = async(req, res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+module.exports = {
+    deleteQuestion,
+    addUpdateQuestion,
+    getQuestions,
+    getSingleQuestion
+}
