4ab0be1ae11993d7fcda73d2979b04bc065c78c5
SandeSydney <sandesydney01@gmail.com>
2022-12-07 10:34:24 +0300

    Added Server instance

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+require("./Config/index")
+
+const app = express()
+
+app.use(express.json())
+
+
+app.listen( process.env.PORT || 4000, ()=>{
+    console.log(`Server Running on port ${process.env.PORT}`);
+})
