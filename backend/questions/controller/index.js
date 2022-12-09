2cb582ad26d0fe67ef422fd27c29cda676085bf7
SandeSydney <sandesydney01@gmail.com>
2022-12-09 06:47:00 +0300

    Set up basic controller

---
+++ backend/questions/controller/index.js
@@ -0,0 +1,38 @@
+const getQuestions = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+const getQuestionById = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+const addUpdateQuestion = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+const deleteQuestion = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+module.exports = {
+    addUpdateQuestion,
+    getQuestions,
+    getQuestionById,
+    deleteQuestion
+}
