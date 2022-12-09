1e66bcac34b72ac92428fa824ccfb8fbf4323153
SandeSydney <sandesydney01@gmail.com>
2022-12-09 14:26:57 +0300

    Update on users server

--- backend/users/index.js
+++ backend/users/index.js
@@ -1,12 +1,12 @@
 const dotenv = require("dotenv").config()
 const express = require("express")
-const router = require("./routes/index")
+const { router } = require("./routes/index")
 
 const port = process.env.PORT
 const app = express()
 app.use(express.json())
 app.use("/users", router)
 
-app.listen(port || 4040, ()=>{
+app.listen(port || 4040, () => {
     console.log(`Server running on port: ${port}`);
 })
