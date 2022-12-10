17a14817d3b09d095e62239c5d91a3cdbba60be6
SandeSydney <sandesydney01@gmail.com>
2022-12-10 18:15:47 +0300

    Added login functionality

--- backend/users/controller/index.js
+++ backend/users/controller/index.js
@@ -2,30 +2,49 @@ const mssql = require("mssql")
 const { sqlConfig } = require("../config/config")
 const { v4 } = require("uuid")
 const bcrypt = require("bcrypt")
+const dotenv = require("dotenv")
+const jwt = require("jsonwebtoken")
 
 const loginUser = async (req, res) => {
     try {
+        const { email, password } = req.body
+        const pool = await mssql.connect(sqlConfig)
+        const user = await (await pool.request()
+            .input("email", email)
+            .execute("usp_GetUser")
+        ).recordset[0]
 
+        if (user) {
+            const comparePass = await bcrypt.compare(password, user.password)
+            if (comparePass) {
+                const { user_id, password, profile_picture, IsDeleted, ...payload } = user
+                const token = jwt.sign(payload, process.env.SECRET)
+                return res.status(200).json({ message: "Login Successful", token })
+            } else{
+                return res.status(400).send({message: "Password Incorrect!"})
+            }
+        } else{
+            return res.status(404).send({message: "User unavailable. Kindly sign up!"})
+        }
     } catch (error) {
-
+        return res.status(404).send({error: error.message})
     }
 }
 
 const signupUser = async (req, res) => {
     try {
-        const { user_id, username, email, profile_picture, password } = req.body
-        const pool = mssql.connect(sqlConfig)
+        const { user_id, username, email, password } = req.body
+        const pool = await mssql.connect(sqlConfig)
         const hashed_pass = await bcrypt.hash(password, 10)
         await pool.request()
             .input("user_id", user_id)
             .input("username", username)
             .input("email", email)
-            .input("profile_picture", profile_picture)
             .input("password", hashed_pass)
             .execute("")
-        res.status(201).json({message: `Successfully added ${user.username}!`})
+        res.status(201).json({ message: `Successfully added ${user.username}!` })
     } catch (error) {
-        res.status(404).send({error: error.message})
+        res.status(404).send({ error: error.message })
     }
 }
 
