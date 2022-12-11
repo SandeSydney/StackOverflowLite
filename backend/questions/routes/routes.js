5bc27a2e1db5a4f3add423d87525442fa7a21d0f
SandeSydney <sandesydney01@gmail.com>
2022-12-11 20:43:11 +0300

    Added routes to see answer and delete

--- backend/questions/routes/routes.js
+++ backend/questions/routes/routes.js
@@ -1,4 +1,4 @@
-const { addUpdateQuestion, getQuestions, getQuestionById, deleteQuestion, getQuestionAnswers, addUpdateAnswer } = require("../controller/index")
+const { addUpdateQuestion, getQuestions, getQuestionById, deleteQuestion, getQuestionAnswers, addUpdateAnswer, getAnswerById, deleteAnswer } = require("../controller/index")
 const router = require("express").Router()
 
 router.get("/", getQuestions)
@@ -7,6 +7,8 @@ router.post("/", addUpdateQuestion)
 router.delete("/:question_id", deleteQuestion)
 
 router.get("/:question_id/answers", getQuestionAnswers)
+router.get("/:question_id/answers/:answer_id", getAnswerById)
 router.post("/:question_id/answers", addUpdateAnswer)
+router.delete("/:question_id/answers/:answer_id", deleteAnswer)
 
 module.exports = { router }
