00da6889c2547a02fea69218895c0e50a976c0b7
SandeSydney <sandesydney01@gmail.com>
2022-12-11 11:01:19 +0300

    added questions route

--- backend/questions/index.js
+++ backend/questions/index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
+const { router } = require("./routes/routes")
 
 const port = process.env.PORT
 const app = express()
 app.use(express.json())
+app.use("/questions", router)
 
 app.listen(port || 3030, ()=>{
     console.log(`Question Service running on port ${port}`);
