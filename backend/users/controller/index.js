6846ebf3924b42d06383ed096e985ba830297ca2
SandeSydney <sandesydney01@gmail.com>
2022-12-13 12:00:25 +0300

    Added func to verify user's token

--- backend/users/controller/index.js
+++ backend/users/controller/index.js
@@ -18,8 +18,8 @@ const loginUser = async (req, res) => {
             const comparePass = await bcrypt.compare(password, user.password)
             if (comparePass) {
                 const { user_id, password, profile_picture, IsDeleted, ...payload } = user
-                const token = jwt.sign(payload, process.env.SECRET, {expiresIn: "1 day"})
-                return res.status(200).json({ message: "Login Successful", token, user_id:user_id})
+                const token = jwt.sign(payload, process.env.SECRET, { expiresIn: "1 day" })
+                return res.status(200).json({ message: "Login Successful", token, user_id: user_id })
             } else {
                 return res.status(400).send({ message: "Password Incorrect!" })
             }
@@ -50,7 +50,25 @@ const signupUser = async (req, res) => {
     }
 }
 
+
+const verifyUser = async (req, res) => {
+    try {
+        const token = req.headers['token']
+        if (!token) {
+            return res.status(401).send({ message: "Access Denied. Supply a token!" })
+        }
+
+        const user = jwt.verify(token, process.env.SECRET)
+        if (user) {
+            return res.status(200).send(true)
+        }
+    } catch (error) {
+        res.status(401).send(false)
+    }
+}
+
 module.exports = {
     loginUser,
-    signupUser
+    signupUser,
+    verifyUser
 }
