c300611fb67193c45eedc1be8f4d2cbc5ed0e55f
SandeSydney <sandesydney01@gmail.com>
2022-12-13 11:59:39 +0300

    Added user verification

--- backend/users/routes/index.js
+++ backend/users/routes/index.js
@@ -1,9 +1,10 @@
-const { loginUser, signupUser } = require("../controller/index")
+const { loginUser, signupUser, verifyUser } = require("../controller/index")
 const {Router} = require("express")
 
 const router = Router()
 router.post("/login", loginUser)
 router.post("/signup", signupUser)
+router.get("/verify-user", verifyUser)
 
 module.exports = {
     router
