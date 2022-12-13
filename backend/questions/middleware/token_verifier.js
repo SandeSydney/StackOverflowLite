3e4a9724f0719b44a966385d8eb6053a08e37899
SandeSydney <sandesydney01@gmail.com>
2022-12-13 14:56:57 +0300

    Getting and verifying tokens

--- backend/questions/middleware/token_verifier.js
+++ backend/questions/middleware/token_verifier.js
@@ -1,16 +1,21 @@
 const dotenv = require("dotenv").config()
 const jwt = require("jsonwebtoken")
+const axios = require("axios")
 
-const token_verifier = (req, res, next) => {
+const token_verifier = async (req, res, next) => {
     try {
         const token = req.headers['Authorization'] || req.headers['X-Auth-Token']
 
-        if(!token){
-            return res.status(401).send({message:"Access Denied. Supply a token!"})
+        if(token){
+            await axios.get("http://localhost:4040/users/verify-user")
+            .then( response=>{
+                if(response.data){
+                    return next()
+                } else{
+                    return res.status(403).send("forbidden")
+                }
+            })
         }
-        
-        const dataDecoded = jwt.verify(token, process.env.SECRET)
-        req.info = dataDecoded
     } catch (error) {
         return res.status(404).send({ error: error.message })
     }
