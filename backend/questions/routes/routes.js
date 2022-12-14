d71c73fee40e497242771fb98ca4b768506a9577
SandeSydney <sandesydney01@gmail.com>
2022-12-14 21:28:51 +0300

    Added middleware

--- backend/questions/routes/routes.js
+++ backend/questions/routes/routes.js
@@ -4,16 +4,16 @@ const router = require("express").Router()
 
 router.get("/", getQuestions)
 router.get("/:question_id", getQuestionById)
-router.post("/", addUpdateQuestion)
-router.delete("/:question_id", deleteQuestion)
+router.post("/",token_verifier, addUpdateQuestion)
+router.delete("/:question_id",token_verifier, deleteQuestion)
 
 router.get("/:question_id/answers", getQuestionAnswers)
 router.get("/:question_id/answers/:answer_id", getAnswerById)
-router.post("/:question_id/answers", addUpdateAnswer)
-router.delete("/:question_id/answers/:answer_id", deleteAnswer)
+router.post("/:question_id/answers",token_verifier, addUpdateAnswer)
+router.delete("/:question_id/answers/:answer_id",token_verifier, deleteAnswer)
 
 router.get("/:question_id/answers/:answer_id/comments", getAnswerComments)
-router.post("/:question_id/answers/:answer_id/comments", addUpdateComment)
-router.delete("/:question_id/answers/:answer_id/comments/:comment_id", deleteComment)
+router.post("/:question_id/answers/:answer_id/comments",token_verifier, addUpdateComment)
+router.delete("/:question_id/answers/:answer_id/comments/:comment_id",token_verifier, deleteComment)
 
 module.exports = { router }
