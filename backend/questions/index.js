dc0e6c6f917c050181d60d83e73c2872f350c6cb
SandeSydney <sandesydney01@gmail.com>
2022-12-17 15:31:53 +0300

    Added cors

--- backend/questions/index.js
+++ backend/questions/index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
 const { router } = require("./routes/routes")
+const cors = require("cors")
 
 const port = process.env.PORT
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use("/questions", router)
 
