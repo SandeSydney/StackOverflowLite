c58b14c83eb99526495c81e46b3be99da6b6d664
SandeSydney <sandesydney01@gmail.com>
2022-12-20 15:54:51 +0300

    Updated the component with user details

--- frontend/src/Components/Questionbox.jsx
+++ frontend/src/Components/Questionbox.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { getUserById } from '../Features/usersSlice'
 import userimg from '../Resources/user.png'
 
 function Questionbox({ question }) {
   const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const qtn_author = question.user_id
+
   const viewAnswers = ()=>{
     navigate("/home/answers")
   }
@@ -12,11 +18,14 @@ function Questionbox({ question }) {
       <div type='button' className='question-box'>
         <div className="user-details">
           <img src={userimg} alt="user profile pic" />
-          <p>John Doe</p>
+          <p>{}</p>
         </div>
         <div className="qtnAns">
           <p className='question'>
             {question.question}
+          </p>
+          <p className='subject'>
+            Subject: {question.subject}
           </p>
           <button className='answer-link' onClick={viewAnswers}>Answers (3)</button>
         </div>
