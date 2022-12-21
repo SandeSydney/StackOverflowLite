43f101b4ea519c3685a2c7b4cf6220ef06b5f0c3
SandeSydney <sandesydney01@gmail.com>
2022-12-21 09:24:37 +0300

    Updates on user's questions

--- frontend/src/Components/MyQuestions.jsx
+++ frontend/src/Components/MyQuestions.jsx
@@ -25,8 +25,25 @@ function MyQuestions() {
 
         if (my_questions.length) {
 
-            questions_content = my_questions.map((question, question_id) => {
-                return <Questionbox key={question_id} question={question} />
+            questions_content = my_questions.map((questionItm) => {
+
+
+                const question_id = questionItm.question_id
+                const username = questionItm.username
+                const user_id = questionItm.user_id
+                const subject = questionItm.subject
+                const question_date = questionItm.question_date
+                const question = questionItm.question
+
+
+                return <Questionbox
+                    key={question_id}
+                    question={question}
+                    question_id={question_id}
+                    username={username}
+                    user_id={user_id}
+                    subject={subject}
+                    question_date={question_date} />
             })
 
         } else {
