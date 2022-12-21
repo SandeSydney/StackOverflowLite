10f3e8e4663f4c8f3337debf2481aafc45766a63
SandeSydney <sandesydney01@gmail.com>
2022-12-21 07:04:55 +0300

    Updated answer form

--- frontend/src/Components/Answerform.jsx
+++ frontend/src/Components/Answerform.jsx
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useDispatch } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { addAnswer } from '../Features/answersSlice'
 
 function Answerform() {
-  const handleSubmit = (event)=>{
+
+  const dispatch = useDispatch()
+
+  const answerRef = useRef(null)
+
+  const { question_id } = useParams()
+
+  const handleSubmit = (event) => {
     event.preventDefault()
+
+    if (answerRef.current.value.trim()) {
+      const answer_details = {
+        answer: answerRef.current.value,
+        upvotes: 0,
+        downvotes: 0,
+        IsValid: 0
+      }
+
+      dispatch(addAnswer(answer_details, question_id))
+
+      answerRef.current.value = ''
+    } else {
+      console.log("Supply an answer first!");
+    }
+
   }
   return (
     <form className='answer-form'>
       <div className="form-element">
-        <textarea name="answer" id="answer-textarea" cols="" rows="2" placeholder='Type your answer here...'></textarea>
+        <textarea ref={answerRef} name="answer" id="answer-textarea" cols="" rows="2" placeholder='Type your answer here...'></textarea>
       </div>
       <div className="formBtns">
         <button className='btnReset' type="reset">Reset</button>
-        <button className='btnSubmit' type="submit" onSubmit={handleSubmit}>Submit</button>
+        <button className='btnSubmit' type="submit" onClick={handleSubmit}>Submit</button>
       </div>
-    </form>
+    </form >
   )
 }
 
