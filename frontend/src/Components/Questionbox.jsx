c377a87fd71065b3745da5e868f3dac59415a2ec
SandeSydney <sandesydney01@gmail.com>
2022-12-21 06:02:49 +0300

    Passed state through navigate

--- frontend/src/Components/Questionbox.jsx
+++ frontend/src/Components/Questionbox.jsx
@@ -1,27 +1,37 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import userimg from '../Resources/user.png'
 
-function Questionbox({ question }) {
+function Questionbox({ question_id, username, user_id, subject, question_date, question }) {
+
+  const question_data = {
+    question_id: `${question_id}`,
+    username: `${username}`,
+    user_id: `${user_id}`,
+    subject: `${subject}`,
+    question_date: `${question_date}`,
+    question: `${question}`
+  }
+
   const navigate = useNavigate()
 
-  const viewAnswers = ()=>{
-    navigate("/home/answers")
+  const viewAnswers = () => {
+    navigate(`/home/question/${question_data.question_id}/answers`, { state: question_data })
   }
   return (
     <>
       <div type='button' className='question-box'>
         <div className="user-details">
           <img src={userimg} alt="user profile pic" />
-          <p>{question.username}</p>
+          <p>{question_data.username}</p>
         </div>
         <div className="qtnAns">
           <p className='question'>
-            {question.question}
+            {question_data.question}
           </p>
           <p className='subject'>
-            Subject: {question.subject}
+            Subject: {question_data.subject}
           </p>
           <button className='answer-link' onClick={viewAnswers}>Answers (3)</button>
         </div>
