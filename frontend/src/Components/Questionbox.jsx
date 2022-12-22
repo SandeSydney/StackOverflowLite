641c4a799175adeae996a5d07f9295def141a00b
SandeSydney <sandesydney01@gmail.com>
2022-12-22 16:31:35 +0300

    Fetch logged in user from the local storage

--- frontend/src/Components/Questionbox.jsx
+++ frontend/src/Components/Questionbox.jsx
@@ -1,9 +1,8 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
 import { useNavigate, useParams } from 'react-router-dom'
 import userimg from '../Resources/user.png'
 
 function Questionbox({ question_id, username, user_id, subject, question_date, question }) {
+  const navigate = useNavigate()
 
   const question_data = {
     question_id: `${question_id}`,
@@ -14,7 +13,7 @@ function Questionbox({ question_id, username, user_id, subject, question_date, q
     question: `${question}`
   }
 
-  const navigate = useNavigate()
+  const isLoggedinUser = JSON.parse(localStorage.getItem('user/user_id'))
 
   const viewAnswers = () => {
     navigate(`/home/question/${question_data.question_id}/answers`, { state: question_data })
@@ -27,12 +26,22 @@ function Questionbox({ question_id, username, user_id, subject, question_date, q
           <p>{question_data.username}</p>
         </div>
         <div className="qtnAns">
-          <p className='question'>
-            {question_data.question}
-          </p>
-          <p className='subject'>
-            Subject: {question_data.subject}
-          </p>
+          <div className="qtnActions">
+            <div>
+              <p className='question'>
+                {question_data.question}
+              </p>
+              <p className='subject'>
+                Subject: {question_data.subject}
+              </p>
+            </div>
+            {(question_data.user_id === isLoggedinUser) ?
+              <div className='actions'>
+                <p>Edit</p>
+                <p>Delete</p>
+              </div> :
+              <div></div>}
+          </div>
           <button className='answer-link' onClick={viewAnswers}>Answers</button>
         </div>
       </div>
